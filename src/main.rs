92b75c7c8cf7bccaff1775c6c1ac36b7be13ea9c
hirotaka <musicfair@gmail.com>
2019-08-10 13:48:34 +0900

    Move

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,15 @@ fn main() {
     s.push_str("!, world!");
 
     println!("{}", s);
+/* -------------------- */
+    let x = 5;
+    let y = x;
+    println!("x = {} y = {}", x, y);
+
+    /* Can't build */
+    /*
+    let s1 = String::from("Hello");
+    let s2 = s1;
+    println!("{}, world!", s1);
+    */
 }
